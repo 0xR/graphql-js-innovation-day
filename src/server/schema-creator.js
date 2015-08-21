90ae0929289a3d5a34de7a4e1b7348eef460dd24
Ruben Oostinga <roostinga@xebia.com>
2015-08-21 15:35:00 +0200

    refactor

--- src/server/schema-creator.js
+++ src/server/schema-creator.js
@@ -6,15 +6,26 @@ import {
   GraphQLList
 } from 'graphql/type';
 
+const isObject = jsObject =>
+  (typeof jsObject === "object" && !Array.isArray(jsObject) && jsObject !== null);
+
+const makeTypeDefObject = (name, type) => ({type, description: `[GENERATED] ${name}`});
+
+const makeTypeDef = (name, jsObject) => {
+  if (isObject(jsObject)) {
+    return makeTypeDefObject(name, objectToSchema(name, jsObject));
+  } else {
+    return makeTypeDefObject(name, GraphQLString);
+  }
+};
+
+
 const objectToFields = jsObject =>
   Object.keys(jsObject)
-    .filter(key => typeof jsObject[key] === 'string')
+    .filter(key => typeof jsObject[key] === 'string' || isObject(jsObject[key]))
     .reduce((acc, key) => ({
       ...acc,
-      [key]: {
-        type: GraphQLString,
-        description: `The ${key} of the product.`,
-      }
+      [key]: makeTypeDef(key, jsObject[key])
     }), {});
 
 const objectToSchema = (name, jsObject) =>
